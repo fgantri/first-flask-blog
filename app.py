ce0990695e6b97005959bbaf1d651dd037f06e02
Fouad Gantri <fouad@gantri.dev>
2024-12-08 00:10:50 +0100

    design data structure and create first route

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    blog_posts = [
+        {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
+        {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
+    # More blog posts can go here...
+    ]
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
